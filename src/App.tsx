61ebd377290fc43c6d37c308258e6708893db324
tyamauchi90 <pecopon24@gmail.com>
2023-06-15 15:42:55 +0900

    dev Chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/chat/Chat";
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       {/* sidebar */}
       <Sidebar />
 
-      {/* home */}
-      {/* <Home /> */}
+      {/* chat */}
+      <Chat />
     </div>
   );
 }
