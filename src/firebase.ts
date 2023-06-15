a43d172bc5899a3a3f2108bdc9e6de071ccffc3d
tyamauchi90 <pecopon24@gmail.com>
2023-06-15 16:10:46 +0900

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtp6sBR9mh0F2JZ9rI6POVDwUEpgDvBJQ",
+  authDomain: "discode-clone-35eae.firebaseapp.com",
+  projectId: "discode-clone-35eae",
+  storageBucket: "discode-clone-35eae.appspot.com",
+  messagingSenderId: "489627881384",
+  appId: "1:489627881384:web:98b0cdcfe6fd12bc51179d",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db};
