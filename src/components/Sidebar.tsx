a42312592195ed42d3f209ff9dcb46aa2356a5cf
tyamauchi90 <pecopon24@gmail.com>
2023-06-15 11:00:16 +0900

    dev Sidebar_finish

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar = () => {
   return (
@@ -30,6 +34,27 @@ const Sidebar = () => {
               <h4>プログラミングチャンネル</h4>
             </div>
             <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>Shincode</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
