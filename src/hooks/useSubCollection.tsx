7a6e9740c51441bfd8740e4d5a2c289738261eb0
tyamauchi90 <pecopon24@gmail.com>
2023-06-21 19:01:53 +0900

    fix code & deploy

---
+++ src/hooks/useSubCollection.tsx
@@ -0,0 +1,61 @@
+import { useAppSelector } from "../app/hooks";
+import { db } from "../firebase";
+import {
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+  Timestamp,
+  QueryDocumentSnapshot,
+  DocumentData,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+interface Messages {
+  timestamp: Timestamp;
+  message: string;
+  user: {
+    uid: string;
+    photo: string;
+    email: string;
+    displayName: string;
+  };
+}
+
+const useSubCollection = (
+  collectionName: string,
+  subCollectionName: string
+) => {
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const [subDocuments, setSubDocuments] = useState<Messages[]>([]);
+
+  useEffect(() => {
+    let collectionRef = collection(
+      db,
+      collectionName,
+      String(channelId),
+      subCollectionName
+    );
+
+    const collectionRefOrderBy = query(
+      collectionRef,
+      orderBy("timestamp", "desc")
+    );
+
+    onSnapshot(collectionRefOrderBy, (snapshot) => {
+      let results: Messages[] = [];
+      snapshot.docs.forEach((doc: QueryDocumentSnapshot<DocumentData>) => {
+        results.push({
+          timestamp: doc.data().timestamp,
+          message: doc.data().message,
+          user: doc.data().user,
+        });
+      });
+      setSubDocuments(results);
+    });
+  }, [channelId]);
+
+  return { subDocuments };
+};
+
+export default useSubCollection;
